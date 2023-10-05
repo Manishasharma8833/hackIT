4ecc121295835ec11ad4611b6c245c4f9b3cf060
Manishasharma8833 <146943172+Manishasharma8833@users.noreply.github.com>
2023-10-05 10:06:56 +0545

    c programmimg

---
+++ C/simple interest.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	float p ,t,r,i;
+	printf("Enter principal,time, rate,:\n");
+	scanf("%f%f%f", &p, &t, &r);
+	i=(p*t*r)/100;
+	printf("interest=%f",1);
+	return 0;
+	
+		
+	}
